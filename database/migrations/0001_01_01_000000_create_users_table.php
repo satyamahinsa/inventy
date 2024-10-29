87dc1d1a7a9e9d357ae4432050b6cfaeed5053cc
gerrardgs <gerrardsebastiangege2005@gmail.com>
2024-10-29 20:42:35 +0700

    Added Role & Database Structure

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Monolog\Handler\RollbarHandler;
 
 return new class extends Migration {
     /**
@@ -16,6 +17,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
